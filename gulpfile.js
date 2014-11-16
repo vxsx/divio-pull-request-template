077ecb9091fe8fdc96feec21db929773e6417dd9
Vadim Sikora <vadim.sikora@gmail.com>
2014-11-16 17:32:40 +0100

    working version, say bye bye to updatable version :\

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var inject = require('gulp-inject');
 
 gulp.task('default', function() {
-    gulp.src('./src/bookmarklet.js')
+    gulp.src('./src/index.html')
         .pipe(inject(gulp.src(['./template.md']), {
             starttag: '/* inject:template.md */',
             endtag: '/* endinject */',
             transform: function (filePath, file) {
                 return '[' + file.contents.toString('utf8').split('\n').map(function (str) {
-                    return '"' + str.replace('"', '\"') + '",';
-                }).join('\n') + '""];';
+                    return "'" + str.replace('"', '\"') + "',";
+                }).join(',') + "''];";
             }
         }))
         .pipe(gulp.dest('.'));
--- src/bookmarklet.js
+++
@@ -1,4 +0,0 @@
-(function () {
-    var elem = document.querySelector('[name="pull_request[body]"]');
-    if (elem) elem.value += /* inject:template.md */''/* endinject */;
-});
