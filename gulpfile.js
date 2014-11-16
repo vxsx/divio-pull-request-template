28d839909e9c599592eac879493788933461a650
Vadim Sikora <vadim.sikora@gmail.com>
2014-11-16 17:54:45 +0100

    simplify things

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,14 @@ gulp.task('default', function() {
             starttag: '/* inject:template.md */',
             endtag: '/* endinject */',
             transform: function (filePath, file) {
-                return '[' + file.contents.toString('utf8').split('\n').map(function (str) {
-                    return "'" + str.replace('"', '\"') + "',";
-                }).join(',') + "''];";
+                return "\'" + file.contents.toString('utf8').split('\n').map(function (str) {
+                    return str.replace(/(')/g, '\\$1').replace(/"/g, '&quot;');
+                }).join('\\n') + "\';";
             }
         }))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/index.html', 'template.md'], ['default']);
 });
